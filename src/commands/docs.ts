aeb2424a24367084377e76f11d07623d52c56375
Mandar1jn <marijnkneppers@gmail.com>
2023-04-03 17:53:25 +0200

    Implemented a docs discovery command * Renamed the docs command to docsearch * Added autocomplete functionality * Upgraded discord.js * Added embed utils

--- src/commands/docs.ts
+++ src/commands/docs.ts
@@ -1,12 +1,24 @@
-import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction, AutocompleteInteraction, Embed } from "discord.js";
 import algoliasearch from "algoliasearch";
 import { categories, SearchHit } from "../types";
+import { getDefaultEmbed } from "../utils";
 
 const client = algoliasearch("7AFBU8EPJU", "4440670147c44d744fd8da35ff652518");
 const index = client.initIndex("astro");
 
-const replaceTags = (input: string): string => {
-	return input.replace("&lt;", '<').replace("&gt;", ">");
+const generateNameFromHit = (hit: SearchHit): string => {
+	return reduce(`${hit.hierarchy.lvl0}: ${hit.hierarchy.lvl1}${hit.hierarchy.lvl2? ` - ${hit.hierarchy.lvl2}` : ''} ${(hit.hierarchy.lvl2 && hit.anchor)? `#${hit.anchor}` : ''}`, 100, "...");
+}
+
+const reduce = (string: string, limit: number, delimiter: string | null): string =>
+{
+
+	if(string.length > limit)
+	{
+		return string.substring(0, limit - (delimiter? delimiter.length : 0)) + (delimiter? delimiter : "");
+	}
+
+	return string;
 }
 
 export default {
@@ -17,7 +29,8 @@ export default {
 			option
 				.setName("query")
 				.setDescription("The query to search for")
-				.setRequired(true))
+				.setRequired(true)
+				.setAutocomplete(true))
 		.addBooleanOption(option =>
 			option.setName("hidden")
 			.setDescription("Wether this should only be shown to you. Defaults to true")
@@ -42,9 +55,9 @@ export default {
 					)),
 	async execute(interaction: ChatInputCommandInteraction) {
 
-		if(interaction.channelId != "916064458814681218")
+		if(interaction.channelId != "1036711421439901758")
 		{
-			const embed = new EmbedBuilder().setTitle("This command is still in beta and can therefor not yet be accessed in this channel").setColor("#FF5D00");
+			const embed = getDefaultEmbed().setTitle("This command is still in beta and can therefor not yet be accessed in this channel");
 			await interaction.reply({embeds: [embed], ephemeral: true});
 
 			return;
@@ -52,123 +65,40 @@ export default {
 
 		await interaction.deferReply({ephemeral: interaction.options.getBoolean("hidden") ?? true});
 
+		let reply: SearchHit;
+
+		const embed = getDefaultEmbed();
+
+		try {
+			reply = await index.getObject(interaction.options.getString("query")!)
+		}
+		catch {
+			embed.setTitle("No results found").setDescription("No result was found for this query. To search for arbitrary strings, please use `/docsearch`.");
+			await interaction.editReply({embeds: [embed]});
+			return;
+		}
+
+		embed.setTitle(generateNameFromHit(reply)).setDescription(`[read more](${reply.url})`);
+
+		await interaction.editReply({embeds: [embed]});
+	},
+	async autocomplete(interaction: AutocompleteInteraction)
+	{
 		const reply = await index.search<SearchHit>(interaction.options.getString("query")!, {
 			facetFilters: [["lang:" + (interaction.options.getString('language') ?? "en")]],
-			highlightPreTag: "**",
-			highlightPostTag: "**",
 			hitsPerPage: 20,
-			snippetEllipsisText: '…',
-			attributesToRetrieve: ["hierarchy.lvl0","hierarchy.lvl1","hierarchy.lvl2","hierarchy.lvl3","hierarchy.lvl4","hierarchy.lvl5","hierarchy.lvl6","content","type","url"],
-			attributesToSnippet: ["hierarchy.lvl1:10","hierarchy.lvl2:10","hierarchy.lvl3:10","hierarchy.lvl4:10","hierarchy.lvl5:10","hierarchy.lvl6:10","content:10"]
-		})
- 
-		const items = reply.hits.map(hit => {
-			const url = new URL(hit.url);
-			if(url.hash == "#overview") url.hash = "";
-
-			return {
-				...hit,
-				url: url.href
-			}
+			distinct: true
 		})
 
-		const categories: categories = {};
+		const hits = reply.hits;
 
-		items.forEach(item => {
-
-			if(!categories[item.hierarchy.lvl0])
-			{
-				categories[item.hierarchy.lvl0] = [];
-			}
-			categories[item.hierarchy.lvl0].push(item);
-		});
-
-		// exclude tutorials
-		delete categories["Tutorials"]
-		
-		const embeds: EmbedBuilder[] = [];
-
-		embeds.push(new EmbedBuilder().setTitle(`Results for "${interaction.options.getString("query")}"`).setColor("#FF5D00"))
-		
-		for(const category in categories)
-		{
-			const embed = new EmbedBuilder()
-				.setTitle(category)
-				.setColor("#FF5D00");
-
-			let body = ""
-
-			let items: {[heading: string]: SearchHit[]} = {};
-
-			for(let i = 0; i < categories[category].length && i < 5; i++)
-			{
-				const item = categories[category][i];
-				if(!item._snippetResult)
-					return;
-
-				if(!items[item.hierarchy[`lvl1`]])
-				{
-					items[item.hierarchy[`lvl1`]] = [];
+		await interaction.respond(
+			hits.map(hit => {
+				return {
+					name: generateNameFromHit(hit),
+					value: hit.objectID
 				}
-
-				items[item.hierarchy[`lvl1`]].push(item);
-			}
-
-			for(const subjectName in items)
-			{
-				const subject = items[subjectName];
-
-				for(let i = 0; i < subject.length; i++)
-				{
-					const item = subject[i];
-
-					let hierarchy = "";
-
-					for(let i = 1; i < 7; i++)
-					{
-						if(item.hierarchy[`lvl${i}`])
-						{
-							let string = (i != 1)? " > " : "";
-
-							string += item.hierarchy[`lvl${i}`]
-
-							hierarchy += string;
-						}
-						else
-						{
-							break;
-						}
-					}
-
-					let result = "";
-
-					if(item._snippetResult)
-					{
-						if(item.type == "content")
-						{
-							result = item._snippetResult.content.value;
-						}
-						else
-						{
-							result = item._snippetResult.hierarchy[item.type].value;
-						}
-
-						body += `[🔗](${item.url}) **${replaceTags(hierarchy)}**\n`
-						body += `[${replaceTags(result.substring(0, 66))}](${item.url})\n`
-					}
-				}
-			}
-
-			embed.setDescription(body)
-
-			embeds.push(embed)
-		}
-
-		if(embeds.length == 1)
-		{
-			embeds[0].setTitle(`No results found for "${interaction.options.getString("query")}"`);
-		}
-
-		await interaction.editReply({embeds: embeds});
+			})
+		)
 	}
 }
---
+++ src/events/applicationCommand.ts
@@ -0,0 +1,26 @@
+import { BaseInteraction, Events, InteractionType } from 'discord.js';
+import { Client } from '../types';
+
+export default {
+	event: Events.InteractionCreate,
+	once: false,
+	async execute(interaction: BaseInteraction) {
+		if(!interaction.isChatInputCommand()) return;
+	
+		const client: Client = interaction.client;
+
+		const command = client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	},
+};
