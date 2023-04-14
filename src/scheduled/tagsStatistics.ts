9099a1331499f4ed2c277ad5755dfcdc08cd3073
Mandar1jn <marijnkneppers@gmail.com>
2023-04-14 18:11:25 +0200

    Add support for scheduled jobs

---
+++ src/scheduled/tagsStatistics.ts
@@ -0,0 +1,68 @@
+import { ForumChannel, TextChannel } from "discord.js";
+import { Client, Tags } from "../types"
+import { getDefaultEmbed } from "../utils/embeds.js";
+
+const GUILD_ID = "830184174198718474";
+const FORUM_ID = "1019713903481081876";
+const OUTPUT_ID = "916064458814681218";
+
+export default {
+	time: "* * * * * *",
+	async execute(client: Client) {
+		const guild = await client.guilds.fetch(GUILD_ID);
+
+		const forum: ForumChannel = await guild.channels.fetch(FORUM_ID) as ForumChannel;
+
+		const date = new Date();
+
+		date.setMonth(date.getMonth() - 1);
+
+		const threads = (await forum.threads.fetchActive()).threads.filter(x => x.createdAt! > date);
+
+		let tags: Tags = {};
+
+		threads.forEach(thread => {
+			thread.appliedTags.forEach(tag => {
+				if(!tags[tag])
+				{
+					tags[tag] = 0;
+				}
+
+				tags[tag] += 1;
+			})
+		})
+
+		const embed = getDefaultEmbed().setTitle("Monthly tags report");
+
+		let description = "";
+
+		for(const tag in tags)
+		{
+			const forumTag = forum.availableTags.find(forumTag => forumTag.id == tag);
+
+			let emoji = "";
+
+			if(forumTag)
+			{
+				if(forumTag.emoji)
+				{
+					if(forumTag.emoji.id)
+					{
+						emoji = `<:${forumTag.emoji.id}> `;
+					}
+					else {
+						emoji = `${forumTag.emoji.name!} `;
+					}
+				}
+
+				description += `${emoji}${forumTag?.name}: ${tags[tag]}\n`
+			}
+		}
+
+		embed.setDescription(description);
+
+		const channel = await client.channels.fetch(OUTPUT_ID)! as TextChannel;
+
+		channel.send({embeds: [embed]});
+	}
+}
