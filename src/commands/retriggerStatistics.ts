1cf558777e5c52f24b0b70398e472e9963a66089
Mandar1jn <marijnkneppers@gmail.com>
2023-06-08 20:20:33 +0200

    Added a command to retrigger support statistics

---
+++ src/commands/retriggerStatistics.ts
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { getDefaultEmbed } from "../utils/embeds.js";
+import TagStatistics from "../scheduled/tagsStatistics.js"
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("retrigger-statistics")
+		.setDescription("Trigger the support statistics outside of it's default schedule"),
+	async execute(interaction: ChatInputCommandInteraction) {
+
+		TagStatistics.execute(interaction.client);
+
+		const embed = getDefaultEmbed().setTitle("Successfully retriggered statistics");
+
+		interaction.reply({embeds: [embed], ephemeral: true});
+	}
+}
