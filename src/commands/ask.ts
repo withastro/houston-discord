9cf78ea71a3178380852a6b4756393de83f4144e
Mandar1jn <marijnkneppers@gmail.com>
2023-09-11 14:50:41 +0200

    First /ask implementation

---
+++ src/commands/ask.ts
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { getDefaultEmbed } from "../utils/embeds.js";
+import TagStatistics from "../scheduled/weeklyStatistics.js"
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("ask")
+		.setDescription("Trigger a message on questions that should be reworded"),
+	async execute(interaction: ChatInputCommandInteraction) {
+
+		const embed = getDefaultEmbed().setTitle("Help us help you!")
+			.setDescription("Hey, I'm not sure how to answer your question. Here's some basic info that can help you provide the info we need to help you!\n\nhttps://hackmd.io/w9Sdod7HQTWYaZsaAcW44g");;
+
+		interaction.reply({embeds: [embed], ephemeral: false});
+	}
+}
