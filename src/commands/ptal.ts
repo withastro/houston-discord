c8d3dff78717bd9bdba1c592131751da0d0b2780
Nate Moore <natemoo-re@users.noreply.github.com>
2023-10-25 11:33:04 -0500

    fix(ptal): ignore github-actions[bot] reviews (#41)

--- src/commands/ptal.ts
+++ src/commands/ptal.ts
@@ -66,7 +66,9 @@ function GetHumanStatusFromPullRequestState(state: PullRequestState): string
 function GetReviewStateFromReview(state: string): PullRequestState
 {
 	switch (state) {
-		case "COMMENTED": return 'REVIEWED'
+		case "COMMENTED":
+		case "DISMISSED": 
+			return 'REVIEWED'
 		case "CHANGES_REQUESTED": return 'CHANGES_REQUESTED'
 		case "APPROVED": return 'APPROVED'
 		default: {
@@ -156,8 +158,8 @@ const generateReplyFromInteraction = async (description: string, github: string,
 				for (let { state: rawState, user, html_url } of reviews) {
 					const id = user?.login;
 					if (!id) continue;
-					// Filter out reviews from the author, they are always just replies.
-					if (id === pr.data.user.login) {
+					// Filter out reviews from the author and GitHub Actions, they aren't relevant
+					if (id === pr.data.user.login || id === 'github-actions[bot]') {
 						continue;
 					}
 					const current = reviewsByUser.get(id);
