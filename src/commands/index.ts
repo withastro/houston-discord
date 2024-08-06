7068c9d97656458a862c07d4796bac5580ce0ea2
Hunter Bertoson <alexanderniebuhr@users.noreply.github.com>
2024-08-06 14:07:56 +0000

    [ci] format

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,8 +3,8 @@ import { default as AskCommand } from './ask.js';
 import { default as DocsCommand } from './docs.js';
 import { default as IssueCommand } from './issue.js';
 import { default as PTALCommand } from './ptal.js';
-import { default as TweetCommand } from './tweet.js';
 import { default as SolvedCommand } from './solved.js';
+import { default as TweetCommand } from './tweet.js';
 // import {default as RetriggerStatisticsCommand} from "./retriggerStatistics";
 
 type CommandList = {
