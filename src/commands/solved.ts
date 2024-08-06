10342861d88fe652c9783321dcdbe34e0d687aaf
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-08-06 09:07:30 -0500

    Created new bot command (#92)
    Co-authored-by: Reuben Tier <64310361+TheOtterlord@users.noreply.github.com>

---
+++ src/commands/solved.ts
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Command } from '../types';
+import { getDefaultEmbed } from '../utils/embeds.js';
+
+const command: Command = {
+	data: new SlashCommandBuilder()
+		.setName('solved')
+		.setDescription('Trigger a message to ask users to mark their issue as solved'),
+	async execute(client) {
+		const embed = getDefaultEmbed()
+			.setTitle('If your issue is resolved, please consider doing the following:')
+			.setDescription(
+				"* From the 3 dot menu in the top-right corner of the post (not the message), edit the tags to include the Solved tag\n* From the same 3 dot menu, select Close Post.Your post will still be available to search and can be re-opened simply by replying in it. It will just be moved down with older posts, so we can more easily focus on issues that still need to be resoled. Thank you."
+			);
+
+		return client.reply({
+			embeds: [embed.toJSON()],
+		});
+	},
+};
+
+export default command;
