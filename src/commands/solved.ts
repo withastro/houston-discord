dc4307e9a74ab4fab09626e08de854dc1c5613de
Henri Fournier <hfournier@intratela.com>
2024-08-06 08:37:19 -0700

    Update solved.ts changed text to: "From the ellipses (3-dot menu)..." (#94)
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

--- src/commands/solved.ts
+++ src/commands/solved.ts
@@ -10,7 +10,7 @@ const command: Command = {
 		const embed = getDefaultEmbed()
 			.setTitle('If your issue is resolved, please consider doing the following:')
 			.setDescription(
-				'* From the 3 dot menu in the top-right corner of the post (not the first message), edit the tags to include the Solved tag.\n* From the same 3 dot menu, select Close Post.\nYour post will still be available to search and can be re-opened simply by replying in it. Closing a post moves it down with older posts, so we can more easily focus on issues that still need to be resovled.\nThank you for your help!.'
+				'* From the ellipses (3-dot menu) in the top-right corner of the post (not the first message), edit the tags to include the Solved tag.\n* From the same ellipses, select Close Post.\nYour post will still be available to search and can be re-opened simply by replying in it. Closing a post moves it down with older posts, so we can more easily focus on issues that still need to be resovled.\nThank you for your help!'
 			);
 
 		return client.reply({
