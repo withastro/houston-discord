574b1eed97ed42c0fd195ced48d6c3b570ff0c3d
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-08-06 08:58:48 -0500

    Update src/commands/solved.ts
    Co-authored-by: Reuben Tier <64310361+TheOtterlord@users.noreply.github.com>

--- src/commands/solved.ts
+++ src/commands/solved.ts
@@ -11,8 +11,8 @@ const command: Command = {
 			.setTitle('If your issue is resolved, please consider doing the following:')
 			.setDescription(
 				`
-                * From the 3 dot menu in the top-right corner of the post (not the message), edit the tags to include the Solved tag\n
-                * From the same 3 dot menu, select Close Post.Your post will still be available to search and can be re-opened simply by replying in it. It will just be moved down with older posts, so we can more easily focus on issues that still need to be resoled. Thank you.`
+* From the 3 dot menu in the top-right corner of the post (not the message), edit the tags to include the Solved tag\n
+* From the same 3 dot menu, select Close Post.Your post will still be available to search and can be re-opened simply by replying in it. It will just be moved down with older posts, so we can more easily focus on issues that still need to be resoled. Thank you.`
 			);
 
 		return client.reply({
