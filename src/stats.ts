819ddb49c5447eaf10dbb5c5f83236fb7829ef8e
Reuben Tier <64310361+TheOtterlord@users.noreply.github.com>
2024-08-12 18:24:49 +0100

    Weekly statistics via GitHub Action (#96)

---
+++ src/stats.ts
@@ -0,0 +1,15 @@
+import { Client } from 'discord.js'
+import stats from './scheduled/weeklyStatistics'
+import { config } from 'dotenv'
+
+config()
+
+const client = new Client({
+  intents: ['GuildMessages', 'MessageContent']
+})
+
+await client.login(process.env.DISCORD_TOKEN)
+
+await stats.execute(client)
+
+await client.destroy()
