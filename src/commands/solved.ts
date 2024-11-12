ae43d3b68fa0414461ef819fb74f3a31a334a05d
Armand Philippot <git@armand.philippot.eu>
2024-11-12 21:16:23 +0100

    fix: tiny typo in `/solved` command (#102)

--- src/commands/solved.ts
+++ src/commands/solved.ts
@@ -10,7 +10,7 @@ const command: Command = {
 		const embed = getDefaultEmbed()
 			.setTitle('If your issue is resolved, please help by doing the following two steps:')
 			.setDescription(
-				'1. From the ellipses (3-dot menu) in the top-right corner of the post (not the first message), edit the tags to include the Solved tag.\n2. From the same ellipses, select Close Post.\nYour post will still be available to search and can be re-opened simply by replying in it. Closing a post moves it down with older posts, so we can more easily focus on issues that still need to be resovled.\nThank you for your help!'
+				'1. From the ellipses (3-dot menu) in the top-right corner of the post (not the first message), edit the tags to include the Solved tag.\n2. From the same ellipses, select Close Post.\nYour post will still be available to search and can be re-opened simply by replying in it. Closing a post moves it down with older posts, so we can more easily focus on issues that still need to be resolved.\nThank you for your help!'
 			);
 
 		return client.reply({
